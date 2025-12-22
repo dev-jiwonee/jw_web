5c5817b48e970d41c2a5b365298c0d21928a3008
jiwon <dev.jiwonee@gmail.com>
2025-12-22 12:23:22 +0900

    코드 정리

--- app/coming-soon/page.tsx
+++ app/coming-soon/page.tsx
@@ -1,5 +1,7 @@
 export default function Page() {
-    return (
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center">준비중</div>
-    );
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1>🚧 준비중</h1>
+    </div>
+  );
 }
