4773b03fd3392876cf6e3083ebe116b5baa3eae1
jiwon <dev.jiwonee@gmail.com>
2026-01-13 15:02:49 +0900

    feat: 메인 스크린 컴포넌트 추가, 전체 레이아웃 구성

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
+import Footer from "@/compoents/blocks/Footer";
+import Header from "@/compoents/blocks/Header";
+import MainScreen from "@/compoents/screens/MainScreen";
 import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect("/coming-soon");
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        main
+    <main className="flex flex-col w-full h-dvh">
+      <Header />
+      <MainScreen />
+      <div id="about" className="min-h-full bg-red-200">
+        내 소개, 일한 경력, 자격증, 장점 ? 가능 스킬
+      </div>
+      <div id="portfolio" className="min-h-full bg-red-300">
+        portfolio
+      </div>
+      <div id="exp" className="min-h-full bg-red-400">
+        experiences
+      </div>
+      <div id="contact" className="min-h-full bg-red-400">
+        contact
+      </div>
+
+      <Footer />
       </main>
-    </div>
   );
 }
---
+++ compoents/screens/MainScreen.tsx
@@ -0,0 +1,13 @@
+const MainScreen = () => {
+  return (
+    <div id="home" className="min-h-full animate-bg-gradient">
+      <div className="relative z-1 flex items-center justify-center h-full">
+        <div className="backdrop-blur-xl bg-white/40 border border-white/60 shadow-lg rounded-3xl p-10 text-gray-800 text-center">
+          <h1 className="text-4xl font-bold">지원's 포트폴리오</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainScreen;
