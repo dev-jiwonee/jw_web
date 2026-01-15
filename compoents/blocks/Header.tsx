9d75b0c3b2e982c1b6ed9c899d2d11cded60c89e
jiwon <dev.jiwonee@gmail.com>
2026-01-15 16:42:41 +0900

    style: header 디자인 적용

--- compoents/blocks/Header.tsx
+++ compoents/blocks/Header.tsx
@@ -1,21 +1,21 @@
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 bg-white/70 z-10 flex items-center justify-between h-25 px-12 ">
-      <h1 className="text-2xl font-bold">
+    <header className="fixed top-0 left-0 right-0 flex items-center justify-between h-25 px-15 border-b border-white/30 shadow bg-white/50 backdrop-blur-sm font-pcp font-bold z-10">
+      <h1 className="text-2xl tracking-wide">
         <a href="#home">JIWON's Portfolio</a>
       </h1>
-      <nav className="flex items-center gap-x-10 text-xl">
-        <a href="#about" className="inline-block px-2 hover:bg-amber-50">
-          ABOUT
+      <nav className="flex items-center gap-x-15 text-xl">
+        <a href="#aboutMe" className="px-2 opacity-80 hover:opacity-100 hover:text-pink-500 transition">
+          About Me
         </a>
-        <a href="#portfolio" className="inline-block px-2 hover:bg-amber-50">
-          PORTFOLIO
+        <a href="#skill" className="px-2 opacity-80 hover:opacity-100 hover:text-green-500 transition">
+          Skill
         </a>
-        <a href="#exp" className="inline-block px-2 hover:bg-amber-50">
-          EXPERIENCE
+        <a href="#work" className="px-2 opacity-80 hover:opacity-100 hover:text-orange-500 transition">
+          Work
         </a>
-        <a href="#contact" className="inline-block px-2 hover:bg-amber-50">
-          CONTACT
+        <a href="#contact" className="px-2 opacity-80 hover:opacity-100 hover:text-blue-500 transition">
+          Contact
         </a>
       </nav>
     </header>
