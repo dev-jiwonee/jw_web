a83ea14790bc4ca74798e5a9ce0a1e949256a1f1
jiwon <dev.jiwonee@gmail.com>
2026-01-15 17:34:30 +0900

    style: footer 디자인 적용

--- compoents/blocks/Footer.tsx
+++ compoents/blocks/Footer.tsx
@@ -1,9 +1,14 @@
 const Footer = () => {
   return (
-    <footer>
-      © 2026 Jiwon Lee. All rights reserved.
+    <footer className="flex items-center justify-between h-15 px-15 bg-white font-light text-base">
+      <p>© 2026 Jiwon Lee. All rights reserved.</p>
+      <div className="flex items-center gap-6">
+        <a href="#">RESUME</a>
+        <a href="#">NOTION</a>
+        <a href="#">GITHUB</a>
+      </div>
     </footer>
   );
-}
+};
 
 export default Footer;
