2b3570a75ff6dc9f70bc9dbd87cbb6bc2bf48315
jiwon <dev.jiwonee@gmail.com>
2026-01-19 16:52:00 +0900

    Skills 화면 내용 추가

--- components/screens/SkillsScreen.tsx
+++ components/screens/SkillsScreen.tsx
@@ -1,13 +1,59 @@
+import { SKILLS } from "@/constants/skills";
 import SectionLayout from "../blocks/SectionLayout";
+import Image from "next/image";
 
 const SkillsScreen = () => {
+  const { dev, design, tools } = SKILLS;
+
   return (
-    <SectionLayout
-      id="skills"
-      title="Skills"
-      bgColor="bg-green-50/50"
-    ></SectionLayout>
+    <SectionLayout id="skills" title="Skills" bgColor="bg-green-50/50">
+      <div className="space-y-18">
+        <SkillBlock title="개발" items={dev} />
+        <SkillBlock title="디자인 / UI" items={design} />
+        <SkillBlock title="개발환경 / 협업 툴" items={tools} />
+      </div>
+    </SectionLayout>
   );
 };
 
 export default SkillsScreen;
+
+//  skill block
+interface SkillItem {
+  icon?: string;
+  name: string;
+  desc?: string;
+}
+interface SkillBlockProps {
+  title?: string;
+  items: SkillItem[];
+}
+const SkillBlock = ({ title, items }: SkillBlockProps) => {
+  return (
+    <div className="flex gap-x-11">
+      <div className="w-80 flex-none font-semibold text-2xl">{title}</div>
+      <div className="w-full grid grid-cols-3 gap-8">
+        {items.map((item, idx) => (
+          <div key={idx} className="flex items-start gap-x-1.5">
+            <div className="w-6 h-6 bg-zinc-100">
+              {item.icon && (
+                <Image
+                  src={`/images/${item.icon}`}
+                  width={24}
+                  height={24}
+                  alt={item.name}
+                />
+              )}
+            </div>
+            <div>
+              <p className="font-semibold text-lg leading-[1.4]">{item.name}</p>
+              {item.desc && (
+                <p className="text-xs text-zinc-500">{item.desc}</p>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ constants/skills.ts
@@ -0,0 +1,36 @@
+export const SKILLS = {
+  dev: [
+    {
+      icon: "img_html5.png",
+      name: "HTML5",
+    },
+    { name: "CSS3" },
+    { name: "JavaScript" },
+    { name: "React" },
+    { name: "Vue.js" },
+    { name: "Next.js" },
+    { name: "Gatsby" },
+    { name: "Tailwind Css" },
+    { name: "SCSS" },
+    { name: "Styled-components" },
+    { name: "Vuetify" },
+    { name: "Rest API" },
+    { name: "Firebase" },
+  ],
+  design: [
+    {
+      name: "Figma",
+    },
+    { name: "Adobe Photoshop" },
+    { name: "Sketch" },
+    { name: "Zeplin" },
+  ],
+  tools: [
+    {
+      name: "VS Code",
+    },
+    { name: "Git" },
+    { name: "Notion" },
+    { name: "Slack" },
+  ],
+};
