b7853083d327f10fda3d0cd531de4bbfbb664cc7
jiwon <dev.jiwonee@gmail.com>
2026-01-27 11:27:30 +0900

    footer - 반응형 스타일 코드 추가

--- components/blocks/Footer.tsx
+++ components/blocks/Footer.tsx
@@ -1,11 +1,16 @@
+import { SNS_LINK } from "@/constants/snsLink";
+
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-between h-15 px-15 bg-white font-light text-base">
-      <p>© 2026 Jiwon Lee. All rights reserved.</p>
-      <div className="flex items-center gap-6">
-        <a href="#">RESUME</a>
-        <a href="#">NOTION</a>
-        <a href="#">GITHUB</a>
+    <footer className="flex items-center justify-between h-11 sm:h-14 px-4 md:px-5 lg:px-8 xl:px-15 bg-white font-light text-xxs sm:text-sm text-zinc-600">
+      <p className="">© 2026 Jiwon Lee. All rights reserved.</p>
+      <div className="hidden md:flex items-center gap-6">
+        <a href={SNS_LINK.notion} target="_blank" rel="noreferrer">
+          Notion
+        </a>
+        <a href={SNS_LINK.github} target="_blank" rel="noreferrer">
+          Github
+        </a>
       </div>
     </footer>
   );
