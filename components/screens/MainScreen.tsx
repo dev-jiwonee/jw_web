5b2f8e3498847b8b51ec5fec8e4d76a1e53e01b3
jiwon <dev.jiwonee@gmail.com>
2026-01-27 14:16:30 +0900

    main - 스타일 코드 추가

--- components/screens/MainScreen.tsx
+++ components/screens/MainScreen.tsx
@@ -2,7 +2,7 @@ import IconMouseWheel from "../common/IconMouseWheel";
 
 const MainScreen = () => {
   return (
-    <section id="home" className="min-h-dvh flex items-center justify-center px-4 xl:px-15 animate-bg-gradient">
+    <section id="home" className="relative min-h-dvh flex items-center justify-center px-4 xl:px-15 animate-bg-gradient">
       <div className="mb-10">
         <p className="mb-1 text-base md:text-lg">안녕하세요 :)</p>
         <p className="md:mb-2 text-2xl md:text-[44px] xl:text-5xl/15">웹 퍼블리셔로 시작해,<br/>사용자 경험을 중심으로 웹을 만들어가는</p>
