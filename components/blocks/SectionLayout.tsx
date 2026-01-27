de4c37f152df3bab759d3ed438937b1223d6748f
jiwon <dev.jiwonee@gmail.com>
2026-01-27 11:50:18 +0900

    section 레이아웃 반응형 스타일 코드 추가

--- components/blocks/SectionLayout.tsx
+++ components/blocks/SectionLayout.tsx
@@ -16,9 +16,9 @@ const SectionLayout = ({
   bgColor = "bg-white",
 }: SectionLayoutProps) => {
   return (
-    <section id={id} className={`${bgColor} min-h-dvh py-25 ${className}`}>
-      <div className="w-full max-w-[1400] mx-auto pt-15">
-        <h2 className="mb-10 font-bold text-4xl">{title}</h2>
+    <section id={id} className={`${bgColor} min-h-dvh py-12 md:py-16 lg:py-18 xl:py-20 ${className}`}>
+      <div className="w-full max-w-[1400] mx-auto py-5 lg:py-8 xl:py-10 px-4 md:px-5 lg:px-8 xl:px-15">
+        <h2 className="mb-10 font-bold text-xl lg:text-[28px] xl:text-4xl">{title}</h2>
         {children}
       </div>
     </section>
