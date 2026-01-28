50347e37c6eb2b67890031dd1b070ac21ebea258
jiwon <dev.jiwonee@gmail.com>
2026-01-28 17:45:00 +0900

    main - 레이아웃 수정

--- components/screens/MainScreen.tsx
+++ components/screens/MainScreen.tsx
@@ -1,15 +1,22 @@
+import SectionLayout from "../blocks/SectionLayout";
 import IconMouseWheel from "../common/IconMouseWheel";
 
 const MainScreen = () => {
   return (
-    <section id="home" className="relative min-h-dvh flex items-center justify-center px-4 xl:px-15 animate-bg-gradient">
-      <div className="mb-10">
-        <p className="mb-1 text-base md:text-lg">안녕하세요 :)</p>
-        <p className="md:mb-2 text-2xl md:text-[44px] xl:text-5xl/15">웹 퍼블리셔로 시작해,<br/>사용자 경험을 중심으로 웹을 만들어가는</p>
-        <p className="text-lg md:text-3xl">프론트엔드 개발자 <span className="font-medium">이지원</span> 입니다.</p>
+    <SectionLayout id="home" center className="animate-bg-gradient">
+      <div className="space-y-1.5 lg:space-y-2">
+        <p className="text-sm sm:text-xl lg:text-2xl transition-all">안녕하세요 :)</p>
+        <p className="text-xl sm:text-5xl lg:text-6xl xl:text-7xl leading-[1.4] transition-all">
+          웹 퍼블리셔로 시작해,
+          <br />
+          사용자 경험을 중심으로 웹을 만들어가는
+        </p>
+        <p className="text-lg sm:text-3xl lg:text-4xl transition-all">
+          프론트엔드 개발자 <span className="font-semibold">이지원</span> 입니다.
+        </p>
       </div>
       <IconMouseWheel />
-    </section>
+    </SectionLayout>
   );
 };
 
