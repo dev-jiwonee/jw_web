3689ef244a4c642b3559443d88b3df31de661ed3
jiwon <dev.jiwonee@gmail.com>
2026-01-29 18:00:23 +0900

    Contact - 반응형 스타일 코드 수정

--- components/screens/ContactScreen.tsx
+++ components/screens/ContactScreen.tsx
@@ -5,24 +5,42 @@ import { SNS_LINK } from "@/constants/snsLink";
 
 const ContactScreen = () => {
   return (
-    <SectionLayout id="contact" title="Contact" bgColor="bg-blue-50" className="relative">
-      <div>
-        <p className="mb-7 text-2xl">궁금한 점이 있으시면 언제든 연락주세요</p>
-        <p className="mb-7 font-semibold text-6xl">{PERSONAL_INFO.email}</p>
-        <a
-          href={`mailto:${PERSONAL_INFO.email}`}
-          className="inline-flex items-center gap-1.5 px-8 py-4 bg-white rounded-full text-zinc-600 hover:text-black"
-        >
-          <IconMail />
-          <span className="text-xl">메일 보내기</span>
-        </a>
-        <div className="mt-12 space-x-4.5">
-          <BtnSnsLink iconSrc="/icons/ic_notion.png" label="Notion" href={SNS_LINK.notion} />
-          <BtnSnsLink iconSrc="/icons/ic_github.png" label="Github" href={SNS_LINK.github} />
-          <BtnSnsLink iconSrc="/icons/ic_note.png" label="Dev Note" href={SNS_LINK.dev_note} />
+    <SectionLayout id="contact" title="Contact" bgColor="bg-blue-50">
+      <div className="flex-1 flex flex-col justify-center items-center gap-12 pb-8">
+        <div className="flex flex-col w-full max-w-90 sm:max-w-160 lg:max-w-210">
+          <p className="mb-2.5 sm:mb-4 lg:mb-6 text-sm sm:text-xl lg:text-2xl">
+            궁금한 점이 있으시면 언제든 연락주세요 !
+          </p>
+          <p className="mb-5 sm:mb-8 lg:mb-10 font-semibold text-2xl sm:text-8xl lg:text-9xl">{PERSONAL_INFO.email}</p>
+          <a
+            href={`mailto:${PERSONAL_INFO.email}`}
+            className="inline-flex items-center gap-1.5 sm:gap-2 ml-auto px-5 sm:px-8 lg:px-9 py-3 sm:py-4 lg:py-5 bg-white/80 rounded-full hover:bg-white"
+          >
+            <IconMail className="w-4 h-4 sm:w-5 sm:h-5 lg:w-6 lg:h-6" />
+            <span className="font-medium text-sm sm:text-lg lg:text-xl">메일 보내기</span>
+          </a>
         </div>
-        <p className="absolute bottom-10 left-1/2 -translate-x-1/2 font-light text-xs text-zinc-400">Last updated January 2026</p>
+        <div className="flex flex-wrap gap-2 sm:gap-4">
+          <BtnSnsLink
+            iconSrc="/icons/ic_notion.png"
+            label="Notion"
+            href={SNS_LINK.notion}
+          />
+          <BtnSnsLink
+            iconSrc="/icons/ic_github.png"
+            label="Github"
+            href={SNS_LINK.github}
+          />
+          {/* <BtnSnsLink
+            iconSrc="/icons/ic_note.png"
+            label="Dev Note"
+            href={SNS_LINK.dev_note}
+          /> */}
+        </div>
       </div>
+      <p className="absolute bottom-8 left-1/2 -translate-x-1/2 font-light text-xxs xl:text-xs text-zinc-400">
+        Last updated January 2026
+      </p>
     </SectionLayout>
   );
 };
@@ -40,12 +58,12 @@ const BtnSnsLink = ({ label, iconSrc, href }: BtnSnsLinkProps) => {
       href={href}
       target="_blank"
       rel="noreferrer"
-      className="inline-flex items-center justify-center gap-x-2.5 px-3 py-2 border-2 border-white rounded-full hover:bg-white"
+      className="inline-flex items-center justify-center gap-1 px-4 py-2 border border-white/90 rounded-full hover:bg-white transition-colors"
     >
       {iconSrc && (
-        <img src={iconSrc} alt={label || ""} className="w-5 h-5" />
+        <img src={iconSrc} alt={label || ""} className="w-4 h-4 opacity-90" />
       )}
-      <span className="font-medium">{label}</span>
+      <span className="font-medium text-xs text-zinc-600">{label}</span>
     </a>
   );
 };
