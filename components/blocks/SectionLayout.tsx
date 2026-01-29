f332cb09028370ded86a8bc3eb9bc845dac907c9
jiwon <dev.jiwonee@gmail.com>
2026-01-29 18:04:45 +0900

    section 레이아웃 스타일 수정

--- components/blocks/SectionLayout.tsx
+++ components/blocks/SectionLayout.tsx
@@ -12,7 +12,7 @@ interface SectionLayoutProps {
 const SectionLayout = ({
   id,
   title,
-  children,
+  children,                                                
   className = "",
   bgColor = "bg-white",
   center = false,
@@ -23,10 +23,10 @@ const SectionLayout = ({
       className={`${bgColor} relative flex flex-col h-full min-h-dvh px-4 sm:px-8 lg:px-11 xl:px-15 py-12 sm:py-16 lg:py-18 xl:py-20 ${className}`}
     >
       <div
-        className={`flex-1 flex flex-col w-full max-w-380 mx-auto ${center ? "justify-center items-center" : ""}`}
+        className={`flex-1 flex flex-col w-full max-w-360 mx-auto ${center ? "justify-center items-center" : ""}`}
       >
         {title && (
-          <div className="pt-4 sm:pt-6 lg:pt-10">
+          <div className="pt-4 sm:pt-6 lg:pt-9">
             <h2 className="mb-6 sm:mb-7 lg:mb-8 xl:mb-10 font-bold text-xl sm:text-3xl xl:text-5xl">
               {title}
             </h2>
