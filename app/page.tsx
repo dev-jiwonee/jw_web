a9b105a3ce5ad41a78d4b49042e48134ddb506ec
jiwon <dev.jiwonee@gmail.com>
2026-01-30 13:31:53 +0900

    main 페이지 스타일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
 import Footer from "@/components/blocks/Footer";
 import Header from "@/components/blocks/Header";
+import TopButton from "@/components/common/TopButton";
 import AboutMeScreen from "@/components/screens/AboutMeScreen";
 import ContactScreen from "@/components/screens/ContactScreen";
 import MainScreen from "@/components/screens/MainScreen";
 import SkillsScreen from "@/components/screens/SkillsScreen";
 import WorksScreen from "@/components/screens/WorksScreen";
-import { redirect } from "next/navigation";
-
 export default function Home() {
-  redirect("/coming-soon");
-
   return (
-    <main className="w-full h-dvd font-paperlogy">
+    <main className="relative w-full font-paperlogy">
       <Header />
       <MainScreen />
       <AboutMeScreen />
